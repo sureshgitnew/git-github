113f23d17607bf1951dcb4d304c1027ad4339b47
suresh <sureshbabubandi93@gmail.com>
2020-04-03 15:59:06 +0530

    return commit

--- shell-scripts/8-exit-status.sh
+++ shell-scripts/8-exit-status.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 echo Hai
-exit
+## exit
 echo Bye
 ##exit command by default with 0,
 
 ## we cant use exit command in functon,because it exits the script.so functions also have exit status 
+F() {
+    echo Hai
+    return
+    echo BYE
+}
+F 
+echo function exit status = $?
