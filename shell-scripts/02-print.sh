187cf32937ca0deea825354f4d92fdba70069b38
suresh <sureshbabubandi93@gmail.com>
2020-04-03 09:45:33 +0530

    lines tabesssss

--- shell-scripts/02-print.sh
+++ shell-scripts/02-print.sh
@@ -10,3 +10,13 @@ echo hello
 echo hey
 echo done
 echo next
+
+##instead of using multiple ech0
+#new line(\n) -e
+#new tab (\t)
+#new color (\e) [\e]
+echo -e "Hai\nBye"
+echo -e "Hai\t\t\t\tBye"
+
+
+
