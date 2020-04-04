48e203a68b465b77e34a64a235e4317d33e3db01
suresh <sureshbabubandi93@gmail.com>
2020-04-04 12:52:29 +0530

    demo yml demo

---
+++ shell-scripts/10.if-condition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ACTION=$1
+PACKAGE_NAME=$2
+if[ -f $LoG];then
+rm -f $LoG
+fi
---
+++ shell-scripts/11.stack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+##sudo command check by script 
+##clor code
+R="\e[31m"
+N="\e[0m"
+ID=(id -u)
+if[$ID -ne 0];then
+echo -e "$R script should be run as root user ,sudo access"
+exit 1
+fi
